cabfd3a0dbe3104f1723d61bdd0cd887f7c4b48a
andreymarchenko <andrey-95nn@yandex.ru>
2017-03-09 15:01:38 +0300

    Added second task

--- HomeWork10/src/Servlets/FirstServlet.java
+++ HomeWork10/src/Servlets/FirstServlet.java
@@ -10,13 +10,23 @@ import java.io.IOException;
 @WebServlet("/HomeWork10")
 public class FirstServlet extends HttpServlet {
 
+    public final static String LOGIN = "login";
+    public final static String PASSWORD = "password";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String login = req.getParameter("login");
         String password = req.getParameter("password");
 
-        resp.getWriter().write("Hello," + login);
+        if(login.equals(LOGIN) && password.equals(PASSWORD)) {
+            resp.getWriter().write("<h1>Login and Password are correct</h1>");
+        } else {
+            resp.getWriter().write("<h1>Login and Password are incorrect</h1>");
+        }
+
+
+
 
     }
 }
